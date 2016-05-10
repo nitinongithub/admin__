08014a5bc3a31cbe5ada2a340a8cac49287bcc89
Nitin Deepak <nitin.d12@amrapali.ac.in>
2016-05-10 16:38:05 +0530

    UPDATES
    UPDATES

---
+++ site_admin_panel.sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2016 at 01:05 PM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 7.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `site_admin_panel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bday_data`
+--
+
+CREATE TABLE `bday_data` (
+  `BID` int(11) NOT NULL,
+  `NAME_` varchar(100) NOT NULL,
+  `DOB` varchar(25) NOT NULL,
+  `PHOTO_` varchar(100) NOT NULL,
+  `DOA` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `STATUS` int(11) NOT NULL,
+  `USERNAME_` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `GL_ID` int(11) NOT NULL,
+  `PHOTO_` varchar(250) NOT NULL,
+  `TITLE_` varchar(250) NOT NULL,
+  `WIDTH_` int(11) NOT NULL,
+  `HEIGHT_` int(11) NOT NULL,
+  `CATEG_ID` int(11) NOT NULL,
+  `STATUS` tinyint(1) NOT NULL,
+  `USERNAME_` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery_category`
+--
+
+CREATE TABLE `gallery_category` (
+  `CATEG_ID` int(11) NOT NULL,
+  `CATEGORY` varchar(25) NOT NULL,
+  `DESC` varchar(500) NOT NULL,
+  `STATUS` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `USERNAME_` varchar(40) NOT NULL,
+  `PASSWORD_` varchar(25) NOT NULL,
+  `USER_STATUS` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsevents`
+--
+
+CREATE TABLE `newsevents` (
+  `ID` int(11) NOT NULL,
+  `SUBJECT` varchar(200) NOT NULL,
+  `NEWS` text NOT NULL,
+  `PATH_ATTACH` varchar(150) NOT NULL,
+  `FONTJI` varchar(250) NOT NULL,
+  `DATE_` varchar(25) NOT NULL,
+  `TIME_` varchar(25) NOT NULL,
+  `STATUS` int(11) NOT NULL DEFAULT '1',
+  `USERNAME` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `newsevents`
+--
+
+INSERT INTO `newsevents` (`ID`, `SUBJECT`, `NEWS`, `PATH_ATTACH`, `FONTJI`, `DATE_`, `TIME_`, `STATUS`, `USERNAME`) VALUES
+(1009, 'new1', 'new1', 'x', 'Arial', '09/05/2016', '08:51:10am', 0, 'Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletter`
+--
+
+CREATE TABLE `newsletter` (
+  `NID` int(11) NOT NULL,
+  `TITLE_` varchar(250) NOT NULL,
+  `VOLUME_` int(11) NOT NULL COMMENT 'Means edition (i.e 1,2,3,4...n) of newsletter',
+  `COVER_` varchar(250) NOT NULL,
+  `PATH_` varchar(250) NOT NULL,
+  `DATE_` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `YEAR_` varchar(5) NOT NULL,
+  `USERNAME_` varchar(150) NOT NULL,
+  `STATUS_` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_status`
+--
+
+CREATE TABLE `user_status` (
+  `ST_ID` varchar(5) NOT NULL,
+  `STATUS` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bday_data`
+--
+ALTER TABLE `bday_data`
+  ADD PRIMARY KEY (`BID`),
+  ADD KEY `USERNAME_` (`USERNAME_`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`GL_ID`),
+  ADD KEY `CATEG_ID` (`CATEG_ID`);
+
+--
+-- Indexes for table `gallery_category`
+--
+ALTER TABLE `gallery_category`
+  ADD PRIMARY KEY (`CATEG_ID`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`USERNAME_`),
+  ADD KEY `USER_STATUS` (`USER_STATUS`);
+
+--
+-- Indexes for table `newsevents`
+--
+ALTER TABLE `newsevents`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `STATUS` (`STATUS`);
+
+--
+-- Indexes for table `newsletter`
+--
+ALTER TABLE `newsletter`
+  ADD PRIMARY KEY (`NID`),
+  ADD KEY `USERNAME_` (`USERNAME_`);
+
+--
+-- Indexes for table `user_status`
+--
+ALTER TABLE `user_status`
+  ADD PRIMARY KEY (`ST_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bday_data`
+--
+ALTER TABLE `bday_data`
+  MODIFY `BID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `GL_ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `gallery_category`
+--
+ALTER TABLE `gallery_category`
+  MODIFY `CATEG_ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `newsevents`
+--
+ALTER TABLE `newsevents`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1010;
+--
+-- AUTO_INCREMENT for table `newsletter`
+--
+ALTER TABLE `newsletter`
+  MODIFY `NID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `login`
+--
+ALTER TABLE `login`
+  ADD CONSTRAINT `usrstatus_for_login` FOREIGN KEY (`USER_STATUS`) REFERENCES `user_status` (`ST_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
